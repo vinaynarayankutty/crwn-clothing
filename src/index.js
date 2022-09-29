4d005cbaad3e367676837932fce863c703be59c9
vinaynarayankutty <vinaynarayankutty@gmail.com>
2022-09-29 16:19:47 +0530

    Added stripe and serverless function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
 
 import App from "./App";
 import { persistor, store } from "./store/store";
@@ -10,6 +11,7 @@ import { persistor, store } from "./store/store";
 import "./index.scss";
 
 import reportWebVitals from "./reportWebVitals";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -17,7 +19,9 @@ root.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
